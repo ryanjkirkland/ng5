bc72b5985514f2a5e68e1e7b0f838a0fbeab9cc6
Ryan Kirkland <ryankirklandga@gmail.com>
2018-06-10 16:33:41 -0600

    first commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
